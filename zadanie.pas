1d35a229f94ca6221e67bba6af9d2d713f78a2a2
MaxMuradov <83012647+MaxMuradov@users.noreply.github.com>
2021-10-07 13:33:23 +0300

    Update zadanie.pas

--- zadanie.pas
+++ zadanie.pas
@@ -6,19 +6,12 @@
     case l of
     11,12,13,14:  begin writeln('my nashli ' ,k, ' gribov v lesu');
     exit;
+    end; 
     end;
-
-      
-      
-    end;
-    
     i:= k mod 10;
     case i of
       1: writeln('my nashli', k, ' grib v lesu');
       2,3,4 :writeln('my nashli ' ,k ,' griba v lesu');
       5,6,7,8,9,0: writeln('my nashli ' ,k, ' gribov v lesu');
     end;
-    
-    
-    
   end.

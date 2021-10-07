823273387eeae1ec912b29790d0c2b40ee24614c
MaxMuradov <83012647+MaxMuradov@users.noreply.github.com>
2021-10-07 13:32:14 +0300

    Add files via upload

---
+++ zadanie.pas
@@ -0,0 +1,24 @@
+﻿var
+  k,i,l :integer;
+  begin
+    read (k);
+    l:= k mod 100;
+    case l of
+    11,12,13,14:  begin writeln('my nashli ' ,k, ' gribov v lesu');
+    exit;
+    end;
+
+      
+      
+    end;
+    
+    i:= k mod 10;
+    case i of
+      1: writeln('my nashli', k, ' grib v lesu');
+      2,3,4 :writeln('my nashli ' ,k ,' griba v lesu');
+      5,6,7,8,9,0: writeln('my nashli ' ,k, ' gribov v lesu');
+    end;
+    
+    
+    
+  end.
---
+++ zadanie1.pas
@@ -0,0 +1,22 @@
+﻿type
+cif = (nol,odin,dva,tri,cheture,pyat);
+var
+  d: char;
+  i: char;
+  n: cif;
+begin
+  read(d);
+  n:= nol;
+  for i:= d downto 1 do
+    n:= succ(n);
+case n of
+  nol: writeln(nol);
+  odin: writeln(odin);
+  dva: writeln(dva);
+  tri: writeln(tri);
+  cheture: writeln(cheture);
+  pyat: writeln(pyat);
+    
+    
+    
+end.
